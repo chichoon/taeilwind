a1739b5123cbd8ded23df2c00b8666d2a0b0c260
chichoon <chichoon.choi@gmail.com>
2023-05-17 14:03:37 +0900

    feat: :sparkles: navbar 추가

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,9 +1,9 @@
+import { NavBar } from "@/components";
+
 const Page = () => {
   return (
     <main className="flex w-screen h-screen justify-center items-center">
-      <div className="bg-white px-4 py-1 rounded-sm ring-2 ring-slate-300">
-        안녕하세요
-      </div>
+      <NavBar />
     </main>
   );
 };
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,7 @@
+export const NavBar = () => {
+  return (
+    <nav className="flex-column center center w-20 h-screen fixed top-0 left-0 bg-slate-800">
+      <button className="w-20 h-20">아아</button>
+    </nav>
+  );
+};
