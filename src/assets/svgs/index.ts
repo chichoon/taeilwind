b4135639bf75a3ed78a2154a70ec840486d523e1
chichoon <chichoon.choi@gmail.com>
2023-05-18 21:37:32 +0900

    feat: :sparkles: 아이콘 추가

---
+++ src/assets/svgs/index.ts
@@ -0,0 +1,1 @@
+export { FtIcon } from "./FtIcon";
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,7 +1,10 @@
+import { FtIcon } from "@/assets/svgs";
+
 export const NavBar = () => {
   return (
-    <nav className="flex-column center center w-20 h-screen fixed top-0 left-0 bg-slate-800">
-      <button className="w-20 h-20">jiychoi</button>
+    <nav className="flex-col justify-center items-center w-20 h-screen fixed top-0 left-0 bg-slate-800">
+      <FtIcon className="fill-white w-16 h-16 p-0 m-0" />
+      <button className="w-20 h-20 text-white">jiychoi</button>
     </nav>
   );
 };
