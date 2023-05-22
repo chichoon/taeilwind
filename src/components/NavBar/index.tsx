96d5f2a5b8a5649a0f5a299c5c735922325b5fe1
chichoon <chichoon.choi@gmail.com>
2023-05-22 19:14:41 +0900

    style: navbar fixed 해제

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,8 +2,8 @@ import { FtIcon } from "@/assets/svgs";
 
 export const NavBar = () => {
   return (
-    <nav className="flex-col justify-center items-center w-20 h-screen fixed top-0 left-0 bg-slate-800">
-      <FtIcon className="fill-white w-16 h-16 p-0 m-0" />
+    <nav className="flex-col justify-center items-center w-20 h-screen bg-slate-800">
+      <FtIcon className="fill-white w-14 h-14 p-0 m-0" />
       <button className="w-20 h-20 text-white">jiychoi</button>
     </nav>
   );
