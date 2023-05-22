bf9411c9724bd6f0e9311a922616a7063bbfb768
chichoon <chichoon.choi@gmail.com>
2023-05-22 19:11:53 +0900

    style: section divider

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,9 +1,15 @@
 import { NavBar } from "@/components";
+import { StudentInfo } from "@/components/StudentInfo";
+import { TopBar } from "@/components/TopBar.tsx";
 
 const Page = () => {
   return (
-    <main className="w-screen h-screen">
+    <main className="w-screen h-screen flex flex-row">
       <NavBar />
+      <div className="flex-1">
+        <TopBar />
+        <StudentInfo />
+      </div>
     </main>
   );
 };
