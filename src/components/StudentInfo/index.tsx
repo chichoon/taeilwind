a0176ed5efec5e514e74c7006a39b1503a914b50
chichoon <chichoon.choi@gmail.com>
2023-05-23 00:01:42 +0900

    feat: :sparkles: 학생 정보 패널 추가

---
+++ src/components/StudentInfo/index.tsx
@@ -0,0 +1,9 @@
+export const StudentInfo = () => {
+  return (
+    <section
+      className="w-sc h-72 bg-cover"
+      style={{ backgroundImage: "url(/lee_cover.jpg)" }}>
+      dkd
+    </section>
+  );
+};
