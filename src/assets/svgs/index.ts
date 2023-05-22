d7ae7ed18dd1aa36f9e48ecfbb94a1f6035ec34f
chichoon <chichoon.choi@gmail.com>
2023-05-22 19:35:38 +0900

    chore: :bento: bell, search 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,1 +1,3 @@
 export { FtIcon } from "./FtIcon";
+export { SearchIcon } from "./SearchIcon";
+export { BellIcon } from "./BellIcon";
---
+++ src/components/TopBar.tsx/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+import { BellIcon, SearchIcon } from "@/assets/svgs";
+
+export const TopBar = () => {
+  return (
+    <section className="p-3 pl-6 pr-6 flex flex-row justify-between items-center">
+      <div className="flex flex-row items-center">
+        <SearchIcon className="w-6 h-6 mr-4" />
+        <input type="text" className="border-none" placeholder="search..." />
+      </div>
+      <div className="flex flex-row items-center">
+        <div className="relative w-6 h-6 mr-5">
+          <BellIcon className="w-6 h-6" />
+          <div className="absolute w-5 h-5 rounded-xl flex justify-center items-center bg-red-600 bottom-4 left-3">
+            <span className="text-white text-sm">4</span>
+          </div>
+        </div>
+        <h2 className="font-bold text-gray-600">jiychoi</h2>
+        <Image
+          className="w-10 h-10 ml-5 rounded-full object-cover"
+          width={30}
+          height={30}
+          src="/profile.gif"
+          alt="profile"
+        />
+      </div>
+    </section>
+  );
+};
