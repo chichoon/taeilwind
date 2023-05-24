78eeb0f49ee6990fa79878a45ff65c223b0acaf3
chichoon <chichoon.choi@gmail.com>
2023-05-24 23:57:55 +0900

    feat: :bento: 라벨 아이콘 업데이트

--- src/assets/svgs/LabelIcon.tsx
+++ src/assets/svgs/LabelIcon.tsx
@@ -5,17 +5,27 @@ interface Props {
 export const LabelIcon = ({ className }: Props) => {
   return (
     <svg
-      className={className}
-      width="800px"
-      height="800px"
-      viewBox="0 0 48 48"
-      xmlns="http://www.w3.org/2000/svg">
-      <g id="Layer_2" data-name="Layer 2">
-        <g id="invisible_box" data-name="invisible box">
-          <rect width="48" height="48" fill="none" />
-        </g>
-        <g id="icons_Q2" data-name="icons Q2">
-          <path d="M33.5,9H4a2,2,0,0,0-2,2V37a2,2,0,0,0,2,2H33.5a.9.9,0,0,0,.8-.4L45.6,25.2a2.1,2.1,0,0,0,0-2.4L34.3,9.4a.9.9,0,0,0-.8-.4Z" />
+      className={`${className} coalition-flag--flag`}
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      id="banner"
+      x="0px"
+      y="0px"
+      viewBox="0 0 68 104"
+      // style="enable-background:new 0 0 68 104; fill: #c6c6c6;fill: #bb4140"
+      xmlSpace="preserve">
+      <g id="banner-content">
+        <g
+          id="UI-Intranet-banner-content"
+          transform="translate(-96.000000, -60.000000)">
+          <g
+            id="banner-content-g-1"
+            transform="translate(96.000000, 60.000000)">
+            <polygon
+              id="banner-content-polygon-1"
+              points="0,0 0,80.5 34.3,104 68,80.5 68,0"></polygon>
+          </g>
         </g>
       </g>
     </svg>
