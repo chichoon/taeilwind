07ca3cc04c0de4e4d7793348340ec847eb926fce
chichoon <chichoon.choi@gmail.com>
2023-05-25 00:02:13 +0900

    style: :lipstick: 색상 수정

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -2,8 +2,8 @@ import { FtIcon } from "@/assets/svgs";
 
 export const NavBar = () => {
   return (
-    <nav className="flex-col justify-center items-center w-20 h-screen bg-slate-800">
-      <FtIcon className="fill-white w-14 h-14 p-0 m-0" />
+    <nav className="pt-3 flex flex-col  items-center w-20 h-screen bg-secondary">
+      <FtIcon className="fill-white w-10 h-10 p-0 m-0" />
       <button className="w-20 h-20 text-white">jiychoi</button>
     </nav>
   );
