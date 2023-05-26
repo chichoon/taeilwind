ab7a87aabfa465f9563e82b07fcb792c21a7c698
chichoon <chichoon.choi@gmail.com>
2023-05-26 23:21:59 +0900

    chore: :bento: 위 화살표 아이콘과 트로피 아이콘 추가

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -2,3 +2,5 @@ export { FtIcon } from "./FtIcon";
 export { SearchIcon } from "./SearchIcon";
 export { BellIcon } from "./BellIcon";
 export { LabelIcon } from "./LabelIcon";
+export { AccelIcon } from "./AccelIcon";
+export { TrophyIcon } from "./TrophyIcon";
