6de416c51f7cd4bb017376abe18876a1ae6aa5f8
chichoon <chichoon.choi@gmail.com>
2023-05-27 23:33:12 +0900

    chore: :wrench: 색상 config extend로 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,24 +6,20 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      primary: "#00BABC",
-      secondary: "#292D39",
-      white: "#ffffff",
-      black: "#000000",
-      text: {
-        light: "#A9A9A9",
-        normal: "#666666",
-        dark: "#2c2c34",
-      },
-      tuteur: "#E9BF0F",
-      lee: "#BB4240",
-    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#00BABC",
+        secondary: "#292D39",
+        white: "#ffffff",
+        black: "#000000",
+        black_transparent: "#202026d9",
+        text: {
+          light: "#A9A9A9",
+          normal: "#666666",
+          dark: "#2c2c34",
+        },
+        tuteur: "#E9BF0F",
+        lee: "#BB4240",
       },
     },
   },
