82a46165206da0a09c057aeb6fd24e78d9f64c38
chichoon <chichoon.choi@gmail.com>
2023-05-30 22:35:23 +0900

    feat: :sparkles: 학생 정보 오른쪽 wip

---
+++ src/components/StudentInfo/StudentInfoRight.tsx
@@ -0,0 +1,13 @@
+export const StudentInfoRight = () => {
+  return (
+    <div className="flex-1 flex flex-col h-full gap-3">
+      <div className="bg-black_transparent flex-1 rounded-sm flex flex-col justify-center items-center ">
+        <span>Unavailable</span>
+        <span>-</span>
+      </div>
+      <div className="bg-black_transparent h-10">
+        <span>level 10 - 66%</span>
+      </div>
+    </div>
+  );
+};
