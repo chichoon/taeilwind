52d7f8d2ed5edbd7f65fa8285bb39eba6da11bc2
chichoon <chichoon.choi@gmail.com>
2023-05-30 22:35:23 +0900

    feat: :sparkles: 학생 정보 오른쪽 wip

--- src/components/StudentInfo/index.tsx
+++ src/components/StudentInfo/index.tsx
@@ -1,4 +1,5 @@
 import { StudentInfoLeft } from "./StudentInfoLeft";
+import { StudentInfoRight } from "./StudentInfoRight";
 import { StudentInfoTop } from "./StudentInfoTop";
 
 export const StudentInfo = () => {
@@ -7,8 +8,9 @@ export const StudentInfo = () => {
       className="w-sc h-80 flex flex-col bg-cover px-5 pb-"
       style={{ backgroundImage: "url(/lee_cover.jpg)" }}>
       <StudentInfoTop />
-      <div className="flex flex-row flex-1 items-center">
+      <div className="flex flex-row flex-1 py-2.5 gap-3 items-center">
         <StudentInfoLeft />
+        <StudentInfoRight />
       </div>
     </section>
   );
